8c8fff20902b6dea761eea717d82b534a591fe42
Rinku5020 <5020rinkunimaje@gmail.com>
2024-05-29 16:08:56 +0530

    productPageCompate

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -9,14 +9,43 @@ import Accordion from 'react-bootstrap/Accordion';
 
 function Product() {
     const [data, setData] = useState([])
+    const [sort, setSort] = useState(null)
+    const [filter, setFilter] = useState(null)
+    const [page, setPage] = useState(1)
+    const [Pagelimit,setPageLimit]=useState(1)
+    console.log(sort)
     const fetch = () => {
-        axios.get("http://localhost:8080/ProductData")
-            .then((res) => setData(res.data))
+        axios.get("http://localhost:8080/ProductData",
+            {
+                params:
+                {
+                    _sort: "price",
+                    _order: sort,
+                    description: filter,
+                    _limit:"6",
+                    _page:page
+                }
+            })
+            .then((res) =>{
+                setPageLimit(res.headers["x-total-count"])
+                setData(res.data)
+                
+            } )
             .catch((err) => console.log(err))
     }
     useEffect(() => {
         fetch()
-    }, [])
+    }, [sort, filter,page])
+
+    const handleFilterChange = (filterValue) => {
+        setFilter(filterValue);
+    }
+
+    const clearFilters = () => {
+        setFilter(null);
+    }
+
+
     return (
         <div>
             <Container>
@@ -41,18 +70,21 @@ function Product() {
                     <Col sm={3} className="d-none d-md-block">
 
                         <div className='filterTag'>
-                         <label className='d-flex justify-content-between '  ><p>Filter</p> <p>Clear All</p></label>   
+                            <label className='d-flex justify-content-between'>
+                                <p className='filterP'>FILTERS</p>
+                                <p className='filterP' onClick={clearFilters} style={{ cursor: 'pointer' }}>Clear All</p>
+                            </label>
                             <Accordion defaultActiveKey="0" flush>
-                                <Accordion.Item eventKey="0">
-                                    <Accordion.Header>Brand</Accordion.Header>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header >Brand</Accordion.Header>
                                     <Accordion.Body>
-                                        <h6>Bewakoof</h6>
-                                        <h6>Olivi</h6>
-                                        <h6>bewakoof x marvel</h6>
-                                        <h6>Bewakoof Air 1.0</h6>
+                                        <h6 onClick={() => handleFilterChange('Bewakoof')} style={{ cursor: "pointer" }}>Bewakoof</h6>
+                                        <h6 onClick={() => handleFilterChange('Olivi')} style={{ cursor: "pointer" }}>Olivi</h6>
+                                        <h6 onClick={() => handleFilterChange('bewakoof x marvel')} style={{ cursor: "pointer" }}>bewakoof x marvel</h6>
+                                        <h6 onClick={() => handleFilterChange('Bewakoof Air 1.0')} style={{ cursor: "pointer" }}>Bewakoof Air 1.0</h6>
                                     </Accordion.Body>
                                 </Accordion.Item>
-                                <Accordion.Item eventKey="1">
+                                <Accordion.Item eventKey="2">
                                     <Accordion.Header>Color</Accordion.Header>
                                     <Accordion.Body>
                                         <h6>#Blank</h6>
@@ -61,7 +93,7 @@ function Product() {
                                         <h6>#Blank</h6>
                                     </Accordion.Body>
                                 </Accordion.Item>
-                                <Accordion.Item eventKey="1">
+                                <Accordion.Item eventKey="3">
                                     <Accordion.Header>Size</Accordion.Header>
                                     <Accordion.Body>
                                         <h6>#Blank</h6>
@@ -70,16 +102,7 @@ function Product() {
                                         <h6>#Blank</h6>
                                     </Accordion.Body>
                                 </Accordion.Item>
-                                <Accordion.Item eventKey="1">
-                                    <Accordion.Header>Color</Accordion.Header>
-                                    <Accordion.Body>
-                                        <h6>#Blank</h6>
-                                        <h6>#Blank</h6>
-                                        <h6>#Blank</h6>
-                                        <h6>#Blank</h6>
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey="1">
+                                <Accordion.Item eventKey="4">
                                     <Accordion.Header>Design</Accordion.Header>
                                     <Accordion.Body>
                                         <h6>#Blank</h6>
@@ -88,7 +111,7 @@ function Product() {
                                         <h6>#Blank</h6>
                                     </Accordion.Body>
                                 </Accordion.Item>
-                                <Accordion.Item eventKey="1">
+                                <Accordion.Item eventKey="5">
                                     <Accordion.Header>Sleeve</Accordion.Header>
                                     <Accordion.Body>
                                         <h6>#Blank</h6>
@@ -97,7 +120,7 @@ function Product() {
                                         <h6>#Blank</h6>
                                     </Accordion.Body>
                                 </Accordion.Item>
-                                <Accordion.Item eventKey="1">
+                                <Accordion.Item eventKey="6">
                                     <Accordion.Header>Type</Accordion.Header>
                                     <Accordion.Body>
                                         <h6>#Blank</h6>
@@ -106,7 +129,16 @@ function Product() {
                                         <h6>#Blank</h6>
                                     </Accordion.Body>
                                 </Accordion.Item>
-                                <Accordion.Item eventKey="1">
+                                <Accordion.Item eventKey="7">
+                                    <Accordion.Header>Rating</Accordion.Header>
+                                    <Accordion.Body>
+                                        <h6>#Blank</h6>
+                                        <h6>#Blank</h6>
+                                        <h6>#Blank</h6>
+                                        <h6>#Blank</h6>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="8">
                                     <Accordion.Header>Rating</Accordion.Header>
                                     <Accordion.Body>
                                         <h6>#Blank</h6>
@@ -116,23 +148,22 @@ function Product() {
                                     </Accordion.Body>
                                 </Accordion.Item>
                             </Accordion>
+
                         </div>
                     </Col>
-                    <Col sm={9}>
+                    <Col sm={9} className="main-content">
                         <div className="d-none d-md-block">
                             <div className='d-flex justify-content-end mt-5'>
-                                <lable className="mr-2"> <lable className="boldtag">SORT BY</lable>
-                                    <select className='tag1'>
-                                        <option value="">Populer</option>
-                                        <option value="">New</option>
-                                        <option value="">Price: High to Low</option>
-                                        <option value="">Price: Low to High</option>
+                                <label className="mr-2">
+                                    <span className="boldtag">SORT BY</span>
+                                    <select className='tag1' onChange={(e) => setSort(e.target.value)}>
+                                        <option value="asc">Low To High</option>
+                                        <option value="desc">High To Low</option>
                                     </select>
-                                </lable>
+                                </label>
                             </div>
                             <div>
-                                <img className='p1img' src="https://images.bewakoof.com/uploads/category/desktop/Classic-Fit-T-Shirts_Inside_Desktop-Banner_IK-1706187400.jpg" fluid />
-
+                                <img className='p1img' src="https://images.bewakoof.com/uploads/category/desktop/Classic-Fit-T-Shirts_Inside_Desktop-Banner_IK-1706187400.jpg" alt="Banner" fluid="true" />
                             </div>
                         </div>
                         <div className="container mt-5">
@@ -141,7 +172,7 @@ function Product() {
                                     <div className="col-lg-4 col-md-6 col-sm-12 mb-4" key={el.id}>
                                         <div className="card h-100 border-0 shadow-sm">
                                             <div className="position-relative">
-                                                <img src={el.image} className="card-img-top" alt={el.title} />
+                                                <img src={el.image} className="card-img-top imgCard" alt={el.title} />
                                                 <div className="rate position-absolute bottom-0 end-0 bg-light text-dark p-1 m-2 rounded">
                                                     ⭐ {el.rating}
                                                 </div>
@@ -157,13 +188,15 @@ function Product() {
                                     </div>
                                 ))}
                             </div>
+                            <div className='d-flex justify-content-between'>
+                            <button  className='btnPng' disabled={page == 1} onClick={() => setPage(page - 1)}>Pre</button>
+                            <button className='btnPng' disabled>{page}</button>
+                            <button className='btnPng' disabled={page == Math.ceil(Pagelimit/6)} onClick={() => setPage(page + 1)}>Next</button>
+                            </div>
                         </div>
-
-
                     </Col>
                 </Row>
             </Container>
-
 
         </div>
     )
